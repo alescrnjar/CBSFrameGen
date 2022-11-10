9f6293af1ec127a7f98515b486af955b4d6c0b8d
alescrnjar <a.crnjar@gmail.com>
2022-11-10 14:56:57 +0100

    fixed some variable names

--- src/Peptide_gen.sh
+++ src/Peptide_gen.sh
@@ -4,9 +4,9 @@
 
 nconf=10000 #Number of desired conformations
 
-tempdir=/home/acrnjar/Desktop/TEMP/Peptides_gen # Output directory
+outdir=../example_input # Output directory
 
-rm $tempdir/gen_mdcrd.cpptraj
+rm $outdir/gen_mdcrd.cpptraj
 
 # Loop over index of conformation
 for idx in `seq 0 $((nconf-1))`
@@ -30,7 +30,7 @@ impose UN { {1 6} } { { "N" "CA" "C" "N" '$val0' } { "C" "N" "CA" "C" '$val1' } 
 
 check UN
 
-saveamberparm UN '$tempdir'/peptide.prmtop '$tempdir'/peptide_'$idx'.inpcrd
+saveamberparm UN '$outdir'/peptide.prmtop '$outdir'/peptide_'$idx'.inpcrd
 
 quit
 ' > leap.in
@@ -40,18 +40,18 @@ tleap -s -f leap.in &> /dev/null
 grep -e 'Errors = ' leap.log | grep -v 0 && echo "ERROR in LEaP." | exit
 
 # Append line to cpptraj script
-echo 'trajin '$tempdir'/peptide_'$idx'.inpcrd' >> $tempdir/gen_mdcrd.cpptraj
+echo 'trajin '$outdir'/peptide_'$idx'.inpcrd' >> $outdir/gen_mdcrd.cpptraj
 done 
 
 # Collate all structures into a single trajectory file
-echo "trajout "$tempdir/"all_conformations.mdcrd mdcrd" >> $tempdir/gen_mdcrd.traj
-echo "center @CA,C,N origin" >> $tempdir/gen_mdcrd.traj
-echo "distance dist1 :1@CA :6@CA out $tempdir/dist.dat" >> $tempdir/gen_mdcrd.traj
-echo "hist dist1 bins 50 out $tempdir/hist.dat" >> $tempdir/gen_mdcrd.traj
-echo "go" >> $tempdir/gen_mdcrd.traj
-cpptraj -p $tempdir/peptide.prmtop < $tempdir/gen_mdcrd.traj > /dev/null
+echo "trajout "$outdir/"all_conformations.mdcrd mdcrd" >> $outdir/gen_mdcrd.traj
+echo "center @CA,C,N origin" >> $outdir/gen_mdcrd.traj
+echo "distance dist1 :1@CA :6@CA out $outdir/dist.dat" >> $outdir/gen_mdcrd.traj
+echo "hist dist1 bins 50 out $outdir/hist.dat" >> $outdir/gen_mdcrd.traj
+echo "go" >> $outdir/gen_mdcrd.traj
+cpptraj -p $outdir/peptide.prmtop < $outdir/gen_mdcrd.traj > /dev/null
 
-rm $tempdir/peptide*.inpcrd
+rm $outdir/peptide*.inpcrd
 
-echo "Successfully put $idx structures in $tempdir/all_conformations.mdcrd"
+echo "Successfully put $idx structures in $outdir/all_conformations.mdcrd"
 
